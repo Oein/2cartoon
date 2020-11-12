ff5df9023f19a823a2d401a99bae80f132cfc2c6
Oein <62917247+Oein@users.noreply.github.com>
2020-11-12 17:08:52 +0900

    update - ver beta 20.11.12.5.8

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let fs = require("fs");
+const { encode } = require("punycode");
 let app = express();
 
 let path = __dirname;
@@ -8,21 +9,35 @@ app.get("/" , (req , res) => {
     res.sendFile(path + "/public/html/index.html");
 });
 
-fs.readdir(path + '/public/cartoons' , function(error , filelist) {
-    console.log("cartoons : " + filelist);
+fs.readdir(path + '/public/cartoons' , function(error , filelista) {
+    console.log("cartoons : " + filelista);
 
-    filelist.forEach(fold => {
+    for(let i = 0;i < filelista.length;i++){
+        let folda = filelista[i];
+        let fold = encodeURI(folda);
+
         app.use('/cartoon/' + fold, express.static(path + '/public/cartoons/' + fold));
-        fs.readdir(path + '/public/cartoons/' + fold , function(error , list){
-            list.forEach(element => {
-                app.use('/cartoon/' + fold + "/" + element, express.static(path + '/public/cartoons/' + fold + "/" + element));
-                console.log(path + '/public/cartoons/' + fold + "/" + element + "\t\t at " + '/cartoon/' + fold + "/" + element + "\n\n");
-            });
+        fs.readdir(path + '/public/cartoons/' + folda , function(error , list){
+            let subCartoons = ``;
+
+            for(let i = 0;i < list.length;i++){
+                let element = list[i];
+
+                app.use('/cartoon/' + fold + "/" + element, express.static(path + '/public/cartoons/' + folda + "/" + element));
+                console.log(path + '/public/cartoons/' + folda + "/" + element + "\t\t at " + '/cartoon/' + folda + "/" + element + "\n\n");
+
+                subCartoons = subCartoons + `<p><div><a href="` + '/cartoon/' + fold + "/" + element + `/main.html">` + element + "</a></div></p>"
+            };
+
+            app.get('/cartoon/' + fold + "/subCartoons.html" , (req , res) => {
+                res.send(subCartoons)
+            })
         })
-    });
+    };
 })
 
 app.use('/ctI', express.static(path + '/public/cartoons/image'));
+app.use('/fonts', express.static(path + '/public/fonts'));
 
 app.listen(80 , function() {
     console.log(`Erpress server started on 80 port`);
