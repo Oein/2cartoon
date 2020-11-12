bcab3e1e7e832079cc665d8876da920be453f7df
Oein <62917247+Oein@users.noreply.github.com>
2020-11-13 07:53:26 +0900

    Pushed by auto pusher at 2020-11-13 -  7:53:26.21

--- index.js
+++ index.js
@@ -81,7 +81,8 @@ app.get("/" , (req , res) => {
     if(cartoon_s_count <= 3) {
         let tr = document.createElement('tr');
         for(let i = 0;i < cartoon_s_count;i++){
-            
+            let td = document.createElement('td');
+            let 
         }
     }
 
