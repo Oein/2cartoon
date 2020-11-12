82185c74b62a8066ddb4c3e2fc9bd95e404b81bb
Oein <62917247+Oein@users.noreply.github.com>
2020-11-12 21:23:41 +0900

    update - ver beta 20.11.12.9.23

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function init(){
                     app.use('/cartoon/' + fold + "/" + element, express.static(path + '/public/cartoons/' + folda + "/" + element));
                     console.log(path + '/public/cartoons/' + folda + "/" + element + "\t\t at " + '/cartoon/' + folda + "/" + element + "\n\n");
     
-                    subCartoons = subCartoons + `<p><h1><div><a href="` + '/cartoon/' + fold + "/" + element + `/main.html">` + element + "</a></div></h1></p>"
+                    subCartoons = subCartoons + `<p><h1><div><a href="` + '/cartoon/' + fold + "/" + element + `/main.html">` + decodeURI(element) + "</a></div></h1></p>"
                 };
     
                 app.get('/cartoon/' + fold + "/subCartoons.html" , (req , res) => {
