cfede6079ea63dfe9638c68a9ddb52cea51c98a0
Oein <62917247+Oein@users.noreply.github.com>
2020-11-13 13:33:00 +0900

    Pushed by auto pusher at 2020-11-13 - 13:33:00.25

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.use(function(req , ers , next) {
 function init_today(){
     const todaya = new Date();
 
-    if(todaya.getHours() )
+    if(todaya.getHours() %  == 0)
 }
 
 app.listen(80 , function() {
