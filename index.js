53a64fd5f0d5d82b180cececed37e31bd32c0218
Oein <62917247+Oein@users.noreply.github.com>
2020-11-13 21:33:25 +0900

    Pushed by auto pusher at 2020-11-13 - 21:33:25.23

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function init(){
     
             app.use('/cartoon/' + fold, express.static(path + '/public/cartoons/' + fold));
             fs.readdir(path + '/public/cartoons/' + folda , function(error , list){
-                let subCartoons = ``;
+                let subCartoons = `<style>* {font}</style>`;
     
                 for(let i = 0;i < list.length;i++){
                     let element = list[i];
