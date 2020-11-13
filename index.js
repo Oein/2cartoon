ed7b3211692ad569f23ddbbd48f4de0e8b41480b
Oein <62917247+Oein@users.noreply.github.com>
2020-11-13 23:00:11 +0900

    Pushed by auto pusher at 2020-11-13 - 23:00:11.24

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ let app = express();
 
 let path = __dirname;
 
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/') // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
+    }
+    filename: function (req, file, cb) {
+      cb(null, file.originalname) // cb 콜백함수를 통해 전송된 파일 이름 설정
+    }
+  });
 let upload = multer({ dest: 'uploads/' });
 
 app.get("/" , (req , res) => {
