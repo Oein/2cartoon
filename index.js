5c4fbd0b3f3a5ce57163e6ddf99217ca3b29824e
Oein <62917247+Oein@users.noreply.github.com>
2020-11-13 13:44:11 +0900

    Pushed by auto pusher at 2020-11-13 - 13:44:11.27

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let total = 0;
 let today = 0;
 
 app.get("/" , (req , res) => {
-    fs.readFileSync(path + "/public/html/index.html" , (err , data))
+    fs.readFileSync(path + "/public/html/index.html" , 'utf-8', (err , data))
     res.sendFile(path + "/public/html/index.html");
 });
 
