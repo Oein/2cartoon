17833b0f678820b34690a15686874330fde183c6
Oein <62917247+Oein@users.noreply.github.com>
2020-11-13 23:01:02 +0900

    Pushed by auto pusher at 2020-11-13 - 23:01:02.30

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ app.get("/" , (req , res) => {
     res.sendFile(path + "/public/html/index.html");
 });
 
-app.post('/upload', upload.single('userfile'), function(req, res){
+app.post('/uploada', upload.single('userfile'), function(req, res){
     res.send('Uploaded! : '+req.file); // object를 리턴함
     console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
-  });
+});
 
 app.get("/tt" , (req , res) =>{
     let a = {};
