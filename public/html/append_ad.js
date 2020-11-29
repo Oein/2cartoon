4fca037f5c751cae581f432f6b29995f860abfc4
Oein <62917247+Oein@users.noreply.github.com>
2020-11-29 21:35:48 +0900

    Pushed by auto pusher at 2020-11-29 - 21:35:48.36

--- public/html/append_ad.js
+++ public/html/append_ad.js
@@ -1,7 +1,7 @@
 let c = document.createElement("div");
 let s = document.createElement("style");
 
-s.innerHTML = `iframe {w} `
+s.innerHTML = `iframe {width: 1px;} `
 
 let a = [
     222101664751 , 
