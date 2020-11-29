2da5222151dae62ff9ec0b7b6b408c08b09f8cfe
Oein <62917247+Oein@users.noreply.github.com>
2020-11-29 21:36:32 +0900

    Pushed by auto pusher at 2020-11-29 - 21:36:32.35

--- public/html/append_ad.js
+++ public/html/append_ad.js
@@ -1,7 +1,7 @@
 let c = document.createElement("div");
 let s = document.createElement("style");
 
-s.innerHTML = `iframe {width: 5px;} `
+s.innerHTML = `iframe {width: 20px;} `
 
 let a = [
     222101664751 , 
