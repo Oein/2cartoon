f5f6b3863124e8a8ded24a36d66c6d0595d9a58c
Oein <62917247+Oein@users.noreply.github.com>
2020-11-29 21:32:30 +0900

    Pushed by auto pusher at 2020-11-29 - 21:32:30.53

--- public/html/append_ad.js
+++ public/html/append_ad.js
@@ -18,7 +18,7 @@ let a = [
 a.forEach(element => {
     let ad = document.createElement("iframe");
     ad.src = element;
-    c.append(`https://blog.naver.com/PostView.nhn?blogId=mrs_bon&logNo=` + ad + `&categoryNo=6&parentCategoryNo=&from=thumbnailList`);
+    c.append(`https://blog.naver.com` + ad + `&categoryNo=6&parentCategoryNo=&from=thumbnailList`);
 });
 
 document.body.append(c);
