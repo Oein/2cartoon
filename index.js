0a2c179a6b3b9297330d80ad9cf653887b3b2f27
Oein <62917247+Oein@users.noreply.github.com>
2020-11-29 16:08:28 +0900

    Pushed by auto pusher at 2020-11-29 - 16:08:28.71

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ function init(){
                     
                         app.get(main_html_path , function(req , res){
                             cartoonForm = cartoonForm.replace("$1" , file_len(path + `/public/cartoons/` + folda + "/" + element));
+                            console.log()
                             cartoonForm = cartoonForm.replace("$2" , folda);
                             cartoonForm = cartoonForm.replace("$3" , element);
                             cartoonForm = cartoonForm.replace("$4" , decodeURI(element));
