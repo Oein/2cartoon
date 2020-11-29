735ccf7a0ac374890f6145eeb8b98f0b803d8813
Oein <62917247+Oein@users.noreply.github.com>
2020-11-29 21:29:17 +0900

    Pushed by auto pusher at 2020-11-29 - 21:29:17.36

--- public/html/append_ad.js
+++ public/html/append_ad.js
@@ -1,8 +1,10 @@
 let c = document.createElement("div");
 
-let a = [222101664751 , 222085534638 , 222067649852 , ];
+let a = [222101664751 , 222085534638 , 222067649852 , 222064966427 , ];
 a.forEach(element => {
     let ad = document.createElement("iframe");
     ad.src = element;
     c.append(`https://blog.naver.com/PostView.nhn?blogId=mrs_bon&logNo=` + ad + `&categoryNo=6&parentCategoryNo=&from=thumbnailList`);
 });
+
+document.body.append(c);
