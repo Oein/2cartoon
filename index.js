1c89772464326b0e955a67946ae08a1390c38d65
Oein <62917247+Oein@users.noreply.github.com>
2020-11-29 15:40:54 +0900

    Pushed by auto pusher at 2020-11-29 - 15:40:54.78

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function init(){
             let folda = filelista[i]; //unencoded cartoon name
             let fold = encodeURI(folda); // encoded cartoon name
             app.use('/cartoon/' + fold, express.static(path + '/public/cartoons/' + fold)); //express server opens /cartoon/<EncodedCartoonName>
-            fs.readdir(path + '/public/cartoons/' + folda , function(error , list){  //loop
+            fs.readdir(path + '/public/cartoons/' + folda , function(error , list){  //loop 화's count
                 let subCartoons = `<style>* {font-size: 1.3em;}</style>`;
     
                 for(let i = 0;i < list.length;i++){
