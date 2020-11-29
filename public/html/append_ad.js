ad7b49da555c21447150d73f4f81e211911b7ab4
Oein <62917247+Oein@users.noreply.github.com>
2020-11-29 21:29:04 +0900

    Pushed by auto pusher at 2020-11-29 - 21:29:04.60

--- public/html/append_ad.js
+++ public/html/append_ad.js
@@ -1,10 +1,6 @@
-<iframe src="https://blog.naver.com/PostView.nhn?blogId=mrs_bon&logNo=" />
-<iframe src="https://blog.naver.com/PostView.nhn?blogId=mrs_bon&logNo=222085534638&categoryNo=6&parentCategoryNo=&from=thumbnailList" />
-<iframe src="https://blog.naver.com/PostView.nhn?blogId=mrs_bon&logNo=222067649852&categoryNo=6&parentCategoryNo=&from=thumbnailList" />
-
 let c = document.createElement("div");
 
-let a = [222101664751 , ];
+let a = [222101664751 , 222085534638 , 222067649852 , ];
 a.forEach(element => {
     let ad = document.createElement("iframe");
     ad.src = element;
