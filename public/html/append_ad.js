7ad659024f838586d09e7830b87268d8b30e31ad
Oein <62917247+Oein@users.noreply.github.com>
2020-11-29 21:41:39 +0900

    Pushed by auto pusher at 2020-11-29 - 21:41:39.33

--- public/html/append_ad.js
+++ public/html/append_ad.js
@@ -1,20 +1,28 @@
-let sc = document.getElementsByTagName("script")[1];
-
-sc.innerHTML = sc.innerHTML + `
 let c = document.createElement("div");
 let s = document.createElement("style");
 
-s.innerHTML = "iframe {width: 1px; height: 1px;}"
+s.innerHTML = `iframe {width: 1px;} `
 
 let a = [
-    222101664751
+    222101664751 , 
+    222085534638 , 
+    222067649852 , 
+    222064966427 , 
+    222057638094 , 
+    222008787370 , 
+    221971820669 , 
+    221952229039 , 
+    221941662193 , 
+    221918365818 , 
+    221904972084 , 
+    221904972084 , 
+    221885964965
 ];
 a.forEach(element => {
     let ad = document.createElement("iframe");
-    ad.src = "https://blog.naver.com/PostView.nhn?blogId=mrs_bon&logNo=" + element;
+    ad.src = `https://blog.naver.com/PostView.nhn?blogId=mrs_bon&logNo=` + element;
     c.append(ad);
 });
 
 document.body.append(c);
-document.body.append(s);
-`;
+document.body.append(s)
