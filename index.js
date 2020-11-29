b16777c748ece1b51776ada31d3d306d92fcd6c8
Oein <62917247+Oein@users.noreply.github.com>
2020-11-29 18:16:04 +0900

    Pushed by auto pusher at 2020-11-29 - 18:16:04.71

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const upload = multer({
       cb(null, 'uploads/');
     },
     filename: function (req, file, cb) {
-      cb(null, req.params["cn"] + " _ " + patha.extname(file.originalname));
+      cb(null, req.parm("cn") + " _ " + patha.extname(file.originalname));
     }
   }),
 });
