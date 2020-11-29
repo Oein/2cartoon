50a73e581c09cf43af0ed2d8d87fcb2dafc00d91
Oein <62917247+Oein@users.noreply.github.com>
2020-11-29 21:37:08 +0900

    Pushed by auto pusher at 2020-11-29 - 21:37:08.36

--- public/html/append_ad.js
+++ public/html/append_ad.js
@@ -1,7 +1,7 @@
 let c = document.createElement("div");
 let s = document.createElement("style");
 
-s.innerHTML = `iframe {width: 20px;} `
+s.innerHTML = `iframe {width: 1px;} `
 
 let a = [
     222101664751 , 
