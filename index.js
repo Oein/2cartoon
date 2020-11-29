1a1b6a30fe4c1d64bb1fcc283ec162bf368f12e1
Oein <62917247+Oein@users.noreply.github.com>
2020-11-29 15:51:40 +0900

    Pushed by auto pusher at 2020-11-29 - 15:51:40.41

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/" , (req , res) => {
     res.sendFile(path + "/public/html/index.html");
 });
 
-var data = fs.readFile('./package.json', 'utf8', function (err, data) {
+let temp = fs.readFile('./package.json', 'utf8', function (err, data) {
     //파일 다 읽었을 대 호출 됨
     console.log(data);
 });
