d334a7df0bbe6a3298f55934801f7dc2a9b6f24c
Oein <62917247+Oein@users.noreply.github.com>
2020-11-29 21:28:41 +0900

    Pushed by auto pusher at 2020-11-29 - 21:28:41.26

--- public/html/append_ad.js
+++ public/html/append_ad.js
@@ -1,4 +1,4 @@
-<iframe src="https://blog.naver.com/PostView.nhn?blogId=mrs_bon&logNo=222101664751&categoryNo=6&parentCategoryNo=&from=thumbnailList" />
+<iframe src="https://blog.naver.com/PostView.nhn?blogId=mrs_bon&logNo=222101664751" />
 <iframe src="https://blog.naver.com/PostView.nhn?blogId=mrs_bon&logNo=222085534638&categoryNo=6&parentCategoryNo=&from=thumbnailList" />
 <iframe src="https://blog.naver.com/PostView.nhn?blogId=mrs_bon&logNo=222067649852&categoryNo=6&parentCategoryNo=&from=thumbnailList" />
 
@@ -8,5 +8,5 @@ let a = [];
 a.forEach(element => {
     let ad = document.createElement("iframe");
     ad.src = element;
-    c.append(ad);
+    c.append(`https://blog.naver.com/PostView.nhn?blogId=mrs_bon&logNo=` + ad + `&categoryNo=6&parentCategoryNo=&from=thumbnailList`);
 });
