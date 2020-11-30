f96e959d258287cfd5c793b2f3bddf762976d659
Oein <62917247+Oein@users.noreply.github.com>
2020-11-30 15:27:49 +0900

    Pushed by auto pusher at 2020-11-30 - 15:27:49.61

--- autoupdate.bat
+++ autoupdate.bat
@@ -1,11 +1,11 @@
 @echo off
 
 :a
-git fetch
-git pull
-git add *
-git commit -a -m "Pushed by auto pusher at %date% - %time%"
-git push
+git fetch > temp
+git pull > temp
+git add * > temp
+git commit -a -m "Pushed by auto pusher at %date% - %time%" > temp
+git push > temp
 echo Commited!
 timeout 5
 goto a
