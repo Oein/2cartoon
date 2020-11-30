3247de580f621da28d4fe62c723ee257fafa337a
Oein <Oein0219@gmail.com>
2020-11-30 05:40:25 +0000

    Uploaded!

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/up', upload.array('profile_img'), (req, res) => {
     shell.exec(`cd ` + path);
     shell.exec(`git add *`);
     shell.exec(`git commit -a -m "Uploaded!"`);
-    shell.exec(`git push`);
+    shell.exec(`git push https://Oein:Oein02190219@github.com/Oein/2cartoon.git --all`);
     console.log(`Uploaded!`);
 });
 
