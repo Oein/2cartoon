653ec3e8e2e43ad5dcfa8b5047aece80861287d8
Oein <62917247+Oein@users.noreply.github.com>
2020-12-01 21:18:29 +0900

    Pushed by auto pusher at 2020-12-01 - 21:18:29.49

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ function init(){
                             fs.readdir(path + `/public/cartoons/` + folda + "/" + element, (error, a) => {
                                 let imgs = "";
                                 a.forEach(elemena => {
-                                    imgs = imgs + make_img(`/cartoon/` + folda + "/" + element + "/" + element) + `<p></p>`;
+                                    imgs = imgs + make_img(`/cartoon/` + folda + "/" + element + "/" + elementa) + `<p></p>`;
                                 })
                                 temp = temp.replace("$1" , decodeURI(element));
                                 temp = temp.replace("$2" , imgs);
