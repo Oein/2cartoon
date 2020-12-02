1a1bb31e841f002daad0908d1bb8e209a9cb3813
Oein <62917247+Oein@users.noreply.github.com>
2020-12-02 09:05:55 +0900

    Pushed by auto pusher at 2020-12-02 -  9:05:55.37

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function init(){ //app.get 같은거 하는곳
     ];
     
     let options = [ //id에 맞는 만화리스트
-        "e두환이와포커츄의세계여행/oe대출금리는너굴은행/o",
+        "<option>두환이와포커츄의세계여행/oe대출금리는너굴은행/o",
         "e괴물과귀신이울고있다/o",
         "eTEST/o",
     ];
