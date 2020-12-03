46e7f3217b4c94d2a123268823edfcee884e73ea
Oein <62917247+Oein@users.noreply.github.com>
2020-12-03 21:22:51 +0900

    Pushed by auto pusher at 2020-12-03 - 21:22:50.92

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ function init(){ //app.get 같은거 하는곳
     
         res.send(`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>로그인 실패</title></head><body><h1>Error - 로그인 실패</h1></body>`);
         totalup();
+        req.ip
     });
     
     app.post('/up', upload.array('profile_img'), (req, res) => { //업로드 post
