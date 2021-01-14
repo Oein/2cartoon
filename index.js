07db5d1dd6d8415dbf736cbf2e5f19035d705a60
Oein <62917247+Oein@users.noreply.github.com>
2021-01-14 20:20:07 +0900

    싹다 갈아 엎음 v1.0.0

--- index.js
+++
@@ -1,7 +0,0 @@
-let app = require("express")();
-
-app.get("/" , (req , res) => {
-    res.send("사죄의 말씀 드리며 서버 코드를 처음부터 다시짜고 더 나은 tocar.kro.kr로 돌아오겠습니다. 죄송합니다.");
-})
-
-app.listen(8280);
--- upload.js
+++
@@ -1,8 +0,0 @@
-let shell = require("shelljs");
-
-shell.exec("git add *");
-console.log("a");
-shell.exec("git commit -a -m \"uplaod!\"");
-console.log("b");
-shell.exec("git push");
-console.log("c");
