715e3f349d2b05c35ed2c09c7fb7b92abe5af587
Oein <Oein0219@gmail.com>
2021-01-17 18:01:26 +0900

    Remove html

--- 2cartoon_Pages/src/index.js
+++ 2cartoon_Pages/src/index.js
@@ -2,13 +2,11 @@ let express = require("express");
 let app = express();
 let main = require("./mainPage");
 let view = require("./cartoonViewer");
+let rule = require("./rule");
 let path = require('path');
 
 view.ae(app);
 main.ae(app);
-
-app.get("/rule" , (req , res) => {
-    res.sendFile(path.resolve(__dirname + "/../public/rule.html"))
-});
+rule.ae(app);
 
 app.listen(8280);
