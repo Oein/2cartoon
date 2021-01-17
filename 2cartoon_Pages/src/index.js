270b38183f5aa8c48e8700cf0b3ca3fa191a05c5
Oein <Oein0219@gmail.com>
2021-01-17 13:46:23 +0900

    Really fix error

--- 2cartoon_Pages/src/index.js
+++ 2cartoon_Pages/src/index.js
@@ -2,10 +2,13 @@ let express = require("express");
 let app = express();
 let main = require("./mainPage");
 let view = require("./cartoonViewer");
-let rule = require("./uploadrule");
+let path = require('path');
 
 view.ae(app);
 main.ae(app);
-rule.ae(app);
+
+app.get("/rule" , (req , res) => {
+    res.sendFile(path.resolve(__dirname + "/../public/rule.html"))
+});
 
 app.listen(8280);
--- 2cartoon_Pages/src/uploadrule.js
+++
@@ -1,5 +0,0 @@
-exports.ae = (app) => {
-    app.get("/rule" , (req , res) => {
-        res.sendFile(__dirname + "/../public/rule.html");
-    });
-};
